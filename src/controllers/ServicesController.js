76743731936f99f0a8f4e2b16efd58593bdf8b81
Diogo Soares <diogo.soares10.ds.ds@gmail.com>
2023-11-06 17:52:29 -0300

    folha

--- src/controllers/ServicesController.js
+++ src/controllers/ServicesController.js
@@ -1,12 +1,12 @@
-const knex = require('knex')
+const knex = require('../database')
 
 module.exports = {
     async index(req, res, next) {
         try {
-            const results = await knex('servicos').select('idServico', 'nome', 'preco_p', 'preco_m', 'preco_g')
+            const results = await knex('servicos').select('idServicos', 'nome', 'preco_p', 'preco_m', 'preco_g')
             return res.json(results)
         } catch(error){
-            next(error)
+            next(error)            
         }
-    },
+    }
 }
